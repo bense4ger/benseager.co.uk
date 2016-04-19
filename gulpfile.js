3f2b43bb5d83d75fc08f792552e46abe69e672ff
Ben Seager <github@benseager.co.uk>
2016-04-19 16:45:35 +0100

    Added sass task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,28 @@
 "use strict";
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var sassPath = './scss/**/*.scss';
 var libPaths = ['./lib/**/*.js', './lib/**/*.hbs'];
+var distScriptPath = './dist/scripts';
+var cssPath = './dist/css';
 gulp.task('build-app', function () {
     return browserify('./lib/main.js')
         .bundle()
         .pipe(source('app.js'))
-        .pipe(gulp.dest('./dist/scripts'));
+        .pipe(gulp.dest(distScriptPath));
 });
-gulp.task('build', ['build-app'], function () {
+gulp.task('sass', function () {
+    return gulp.src(sassPath)
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .on('error', sass.logError)
+        .pipe(concat('site.min.css'))
+        .pipe(gulp.dest(cssPath));
+});
+gulp.task('build', ['build-app', 'sass'], function () { });
+gulp.task('default', ['build-app', 'sass'], function () {
     gulp.watch(libPaths, ['build-app']);
+    gulp.watch(sassPath, ['sass']);
 });
-gulp.task('default', ['build-app'], function () { });
