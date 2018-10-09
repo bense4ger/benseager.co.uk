3c0c2ea4f971312df05e748fb0eeb8d3a2bb3c31
Ben Seager <github@benseager.co.uk>
2018-10-09 17:57:00 +0100

    added logo component

---
+++ src/components/logo.vue
@@ -0,0 +1,16 @@
+<template>
+    <img src="../assets/logo.png" alt="logo" />
+</template>
+
+<script>
+    export default {
+        name: 'logo',
+    };
+</script>
+
+<style scoped>
+    img {
+        width: 100%;
+        height: 100%;
+    }
+</style>
