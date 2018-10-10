0f13fcdc3e4e46c3bf52426e46ccf2c959713863
Ben Seager <github@benseager.co.uk>
2018-10-10 20:44:54 +0100

    home view complete

--- src/App.vue
+++ src/App.vue
@@ -27,21 +27,6 @@
 </script>
 
 <style lang="scss">
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-  #nav {
-    padding: 30px;
-    a {
-      font-weight: bold;
-      color: #2c3e50;
-      &.router-link-exact-active {
-        color: $primary-0;
-      }
-    }
-  }
   body{
     background-color: $secondary-2-2;
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,41 @@
 <template>
     <div id="home" class="container">
-        <p>Hello, world!</p>
+        <div class="row">
+            <div class="col-sm">
+                <h2>Hello</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12 col-md-10 offset-md-2">
+                <p>Thanks for stopping by.
+                <br/>
+                <br/>
+                I'm Ben Seager, a full-stack developer based in Norwich</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12 col-md-10 offset-md-2">
+                <p>I'm not wedded to one technology and enjoy using .Net, Node and Go
+                    and I have plenty of experience working on all kinds of projects.
+                    <br/>
+                    <br/>
+                Most of my time is spent in the web (both front and back-end),
+                    and I've developed a real interest in microservices and
+                    serverless architectures.
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12 col-md-10 offset-md-2">
+                <p>Outside of writing code I can be found playing on my
+                    PS4 or indulging my unhealthy relationship with
+                    Norwich City FC.
+                <br/>
+                <br/>
+                I live in the centre of Norwich with my brilliant wife.
+                </p>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -10,8 +45,15 @@
     };
 </script>
 
-<style type="scss" scoped>
-    p {
+<style lang="scss" scoped>
+    #home{
         color: $primary-4;
+        h2 {
+            font-size: 2rem;
+        }
+
+        p {
+            font-size: 1.5rem;
+        }
     }
 </style>
