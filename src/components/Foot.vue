3c7552a7ef82cfe6bd6453a4dd5da334ebaabaaa
Ben Seager <github@benseager.co.uk>
2018-10-14 15:29:54 +0100

    footer complete

---
+++ src/components/Foot.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="row" id="footer">
+        <div class="col">
+            <p>&copy; Ben Seager {{year}}</p>
+        </div>
+        <div class="col offset-sm-5 offset-md-7 offset-lg-9">
+            <social-link href="https://github.com/bense4ger/" icon="github"></social-link>
+            <social-link href="https://twitter.com/digsb" icon="twitter"></social-link>
+            <social-link href="https://www.linkedin.com/in/bdseager" icon="linkedin-in"></social-link>
+        </div>
+    </div>
+</template>
+
+<script>
+    import SocialLink from '@/components/SocialLink.vue';
+
+    export default {
+      name: 'foot',
+      components: {
+        socialLink: SocialLink,
+      },
+      computed: {
+        year() {
+            return new Date().getFullYear();
+        },
+      },
+    };
+
+</script>
+
+<style lang="scss">
+    #footer {
+        background-color: $primary-2;
+        div.col {
+            p {
+                margin-top: 0.75rem;
+            }
+
+            svg.icon {
+                margin-top: 0.5rem;
+                color: $primary-4;
+                margin-left: 1rem;
+            }
+        }
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import '@babel/polyfill';
 import Vue from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTwitter, faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import './plugins/bootstrap-vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+library.add(faTwitter, faGithub, faLinkedinIn);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
